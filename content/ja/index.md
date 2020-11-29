c9abc7f2d317c1935243bfce7c87f8411d7324c7
silloi <silloi1993@gmail.com>
2020-11-29 16:23:26 +0900

    feat: add content config

---
+++ content/ja/index.md
@@ -0,0 +1,7 @@
+---
+title: トップ
+description: 万葉集全文検索
+position: 9999
+category: ホーム
+version: 1.0
+---
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,74 @@
-export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'manyoshu',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+
+  loading: { color: '#48bb78' },
+  generate: {
+    fallback: '404.html', // for Netlify
+    routes: ['/'] // give the first url to start crawling
   },
+  i18n: {
+    locales: () => [{
+      code: 'ja',
+      iso: 'ja_JP',
+      file: 'ja_JP.js',
+      name: '日本語'
+    }, {
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'ja'
+  },
+  // buildModules: [
+  //   ['@nuxtjs/google-analytics', {
+  //     id: 'UA-12301-2'
+  //   }]
+  // ]
+})
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+// export default {
+//   // Global page headers (https://go.nuxtjs.dev/config-head)
+//   head: {
+//     title: 'manyoshu',
+//     meta: [
+//       { charset: 'utf-8' },
+//       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//       { hid: 'description', name: 'description', content: '' }
+//     ],
+//     link: [
+//       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+//     ]
+//   },
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+//   // Global CSS (https://go.nuxtjs.dev/config-css)
+//   css: [
+//   ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+//   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+//   plugins: [
+//   ],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+//   // Auto import components (https://go.nuxtjs.dev/config-components)
+//   components: true,
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-  ],
+//   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+//   buildModules: [
+//     // https://go.nuxtjs.dev/typescript
+//     '@nuxt/typescript-build',
+//   ],
 
-  // Content module configuration (https://go.nuxtjs.dev/config-content)
-  content: {},
+//   // Modules (https://go.nuxtjs.dev/config-modules)
+//   modules: [
+//     // https://go.nuxtjs.dev/content
+//     '@nuxt/content',
+//   ],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+//   // Content module configuration (https://go.nuxtjs.dev/config-content)
+//   content: {},
+
+//   // Build Configuration (https://go.nuxtjs.dev/config-build)
+//   build: {
+//   }
+// }
